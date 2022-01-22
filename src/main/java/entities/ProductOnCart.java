446ba11cf71d81cf1a899859f1ce54fe8912bd65
Jonath Wesley Herdt <jonathherdt@gmail.com>
2022-01-22 02:27:46 -0300

    Creating the entities models for the mongodb collections

---
+++ src/main/java/entities/ProductOnCart.java
@@ -0,0 +1,23 @@
+package entities;
+
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@Data
+@Document(collection = "product_on_cart")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductOnCart {
+	
+	@DBRef
+	private Product product;
+	
+	private Integer quantity;
+}
